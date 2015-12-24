820c460f89bd15ac1e7016d037e18d682185a8bc
akash227 <akash227@yahoo.com>
2015-12-24 15:34:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var app = express();
+
+var passport = require('passport');
+
+var passportLocal = require('passport-local');
+
+
+
+//step 10
+passport.use(new passportLocal.Strategy(function(username,password,done){
+	//pretend this is using a real database
+	if(username === password){
+		console.log("usrname pass equal");
+		return done( null , {id: username, name: username});
+	} else {
+		console.log("usrname pass not equal");
+		done(null,null);
+	}
+}));
+
+
+//step 11
+
+passport.serializeUser(function(user, done){
+	console.log(user.id);
+	done(null , user.id);
+});
+
+passport.deserializeUser(function(id , done){
+	//Query the database or cache here
+	
+	console.log(id);
+	done(null , {id: id,name: id});
+
+});
+
+
+//step 4
+app.set ('view engine','ejs');
+
+
+
+
+//step 8 - npm i body-parser,cookie-parser,express-session
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(expressSession({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+
+//step 7 - passport's 2 middlewares
+app.use(passport.initialize()); //grabs current passport data at a session
+app.use(passport.session()); //puts passport data into current session
+
+
+//step 2
+app.get('/',function(req,res){
+	console.log(req.isAuthenticated()+" "+req.user);
+	res.render('index1',{
+		isAuthenticated: req.isAuthenticated(),
+		user: req.user
+	}); //and create index.ejs
+});
+
+//step 5
+
+app.get('/login',function(req,res){
+	res.render('login'); //create login.ejs
+
+});
+
+//step 6 and step 9
+app.post('/login',passport.authenticate('local'),function(req,res){
+  res.redirect('/');
+});
+
+var port  = 3000;
+
+//step 1
+app.listen(port,function(){
+
+	console.log("Listening to port, %d",port);
+});
